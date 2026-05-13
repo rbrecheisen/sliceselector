870c01c5e97d32a0f4987c5a6ac017005ddc7f89
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-05-13 14:03:31 +0200

    Saving work

--- scripts/runscript.ps1
+++ scripts/runscript.ps1
@@ -5,7 +5,8 @@
 # $SCANS = "M:\data\corpus\06-05-2026\CT scans EMC voor CORPUS study\CT scans EMC 2.0\Deel 4\transfer_3556033_files_756f4039\c4\c4"
 # $SCANS = "M:\data\corpus\12-05-2026\transfer_3571410_files_e8ca9a77\c5 opnieuw\c5"
 # $SCANS = "M:\data\corpus\12-05-2026\transfer_3571410_files_e8ca9a77\Ontbrekende patienten CORPUS study\Ontbrekende patienten CORPUS study"
-$SCANS = "M:\data\corpus\13-05-2026\Ontbrekende patienten CORPUS study - nieuwe scans\Ontbrekende patienten CORPUS study - nieuwe scans"
+# $SCANS = "M:\data\corpus\13-05-2026\Ontbrekende patienten CORPUS study - nieuwe scans\Ontbrekende patienten CORPUS study - nieuwe scans"
+$SCANS = "M:\data\corpus\13-05-2026\EMC176\EMC176"
 
 # $OUTPUT = "M:\data\corpus\06-05-2026\CT scans EMC voor CORPUS study\CT scans EMC 2.0\Deel 1\L3"
 # $OUTPUT = "M:\data\corpus\06-05-2026\CT scans EMC voor CORPUS study\CT scans EMC 2.0\Deel 2\L3"
@@ -13,7 +14,8 @@ $SCANS = "M:\data\corpus\13-05-2026\Ontbrekende patienten CORPUS study - nieuwe 
 # $OUTPUT = "M:\data\corpus\06-05-2026\CT scans EMC voor CORPUS study\CT scans EMC 2.0\Deel 4\L3"
 # $OUTPUT = "M:\data\corpus\12-05-2026\transfer_3571410_files_e8ca9a77\c5 opnieuw\L3"
 # $OUTPUT = "M:\data\corpus\12-05-2026\transfer_3571410_files_e8ca9a77\Ontbrekende patienten CORPUS study\L3"
-$OUTPUT = "M:\data\corpus\13-05-2026\Ontbrekende patienten CORPUS study - nieuwe scans\L3"
+# $OUTPUT = "M:\data\corpus\13-05-2026\Ontbrekende patienten CORPUS study - nieuwe scans\L3"
+$OUTPUT = "M:\data\corpus\13-05-2026\EMC176\L3"
 
 $VERTEBRA = "L3"
 $PATIENT_DIR_IDX = 6
